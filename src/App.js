c756df365801de459a64fec6c781662800a01851
modpage1512 <pacheco1512@gmail.com>
2022-10-07 22:06:56 +0000

    cambio

--- src/App.js
+++ src/App.js
@@ -1,84 +1,38 @@
-import React, {useState, useEffect} from "react";
-import styled from "styled-components";
-
-const BIRD_SIZE = 20;
-const GAME_WIDTH = 500;
-const GAME_HEIGHT = 500;
-const GRAVITY = 3;
-const JUMP_HEIGHT = 100;
-const OBSTACLE_WIDTH = 40;
-const OBSTACLE_GAP = 200;
+import React from "react";
+import "./App.css"
+import "./componentes/datosPersonales.css";
+import {FaBars} from "react-icons/fa"
 
 function App() {
-    const [birdPosition, setBirdPosition] = useState(250)
-    const [gameStarted, setGameStarted] = useState(false);
-    const [obstacleHeight, setObstacleHeight] = useState(100);
-    const [obstacleLeft, setObstacleLeft] = useState(GAME_WIDTH - OBSTACLE_WIDTH)
-
-    const bottomObstacleHeight = GAME_HEIGHT - OBSTACLE_GAP - obstacleHeight;
-
-    useEffect(() => {
-        let timeId;
-        if(gameStarted && birdPosition < GAME_HEIGHT - BIRD_SIZE) {
-            timeId = setInterval(() => {
-                setBirdPosition((birdPosition) => birdPosition + GRAVITY)
-            }, 24)
-        }
-
-        return () => {
-            clearInterval(timeId);
-        }
-    }, [birdPosition, gameStarted])
-
-    const handleClick = () => {
-        let newBirdPosition = birdPosition - JUMP_HEIGHT;
-        if(!gameStarted) {
-            setGameStarted(true)
-        } else if (newBirdPosition < 0){
-            setBirdPosition(0)
-        } else {
-            setBirdPosition(newBirdPosition)
-        }
-    }
-    return ( 
-        <Div onClick={handleClick}>
-            <GameBox height={GAME_HEIGHT} width={GAME_WIDTH}>
-            <Obstacle top={0} width={OBSTACLE_WIDTH} height={obstacleHeight} left={obstacleLeft}/>
-            <Obstacle top={} width={OBSTACLE_WIDTH} height={} left={obstacleLeft}/>
-            <Bird size={BIRD_SIZE} top={birdPosition}/>
-            </GameBox>
-        </Div>
+  
+    return (
+        <div> 
+            <div className="header">
+                <h1>
+                    Nueva tabla
+                </h1>
+                <button className="boton-guardar">Guardar</button>
+            </div>
+            <hr />
+            <div className="body">
+                <article className="datos">
+                    <div>
+                        <h2 className="nombre-titulo">Nombre</h2>
+                        <input className="nombre-input" placeholder="Person"></input>                       
+                    </div>
+                    <div>
+                        <h2 className="descripcion-titulo">Descripcion</h2>
+                        <textarea className="descripcion-input" placeholder="Tabla usada para x y z"></textarea>
+                    </div>
+                </article>
+                <article className="Atributos">
+                    
+                </article>
+                <hr />
+            </div>
+        </div>
     )
 }
 
 export default App;
 
-const Bird = styled.div`
-    position: absolute;
-    background-color: red;
-    height: ${(props) => props.size}px;
-    width: ${(props) => props.size}px;
-    top: ${(props) => props.top}px;
-    border-radius: 50%;
-`;
-
-const Div = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: center;
-`;
-
-const GameBox = styled.div`
-    height: ${(props) => props.height}px;
-    width: ${(props) => props.width}px;
-    background-color: blue;
-`;
-
-const Obstacle = styled.div`
-    position: relative;
-    top: ${(props) => props.top}px;
-    background-color: green;
-    width: ${(props) => props.width}px;
-    height: ${(props) => props.height}px;
-    left: ${(props) => props.left}px;
-`
